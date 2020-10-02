89f634a6221f7132fd5d1006f0edde7865fb72d6
christinayan01 <christinayan01@users.noreply.github.com>
2020-10-02 19:50:36 +0900

    メンバ変数化

--- UE4ShiftLensCamera/MyLocalPlayer.h
+++ UE4ShiftLensCamera/MyLocalPlayer.h
@@ -1,4 +1,5 @@
 // Fill out your copyright notice in the Description page of Project Settings.
+// Created by christinayan01 by Takahiro Yanai. 2020.10.2
 
 #pragma once
 
@@ -14,12 +15,20 @@ class SHIFTLENS1_API UMyLocalPlayer : public ULocalPlayer
 {
 	GENERATED_BODY()
 
-		FSceneView* CalcSceneView(class FSceneViewFamily* ViewFamily,
-			FVector& OutViewLocation,
-			FRotator& OutViewRotation,
-			FViewport* Viewport,
-			class FViewElementDrawer* ViewDrawer = NULL,
-			EStereoscopicPass StereoPass = eSSP_FULL) override;
+	FSceneView* CalcSceneView(class FSceneViewFamily* ViewFamily,
+		FVector& OutViewLocation,
+		FRotator& OutViewRotation,
+		FViewport* Viewport,
+		class FViewElementDrawer* ViewDrawer = NULL,
+		EStereoscopicPass StereoPass = eSSP_FULL) override;
+
+	UMyLocalPlayer() : _shiftLensValue(0.0), _flag(false) {}
+
+	bool _flag;
+
+	// あおり補正　シフトレンズカメラの値。範囲は -1.0～0.0　です
+	double _shiftLensValue;
+	
 
 protected:
 	//virtual void BeginPlay() override;
